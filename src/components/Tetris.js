9c2872b14eb161bf027e7a840e4fff4e6f291e4e
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2021-01-03 12:15:12 +0000

    Added rotate tetromino functionality

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -10,7 +10,7 @@ import { useStage } from '../hooks/useStage';
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
-    const [player, updatePlayerPos, resetPlayer] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
     const [stage, setStage] = useStage(player, resetPlayer)
 
     console.log('re-render')
@@ -52,6 +52,8 @@ const Tetris = () => {
                 movePlayer(1);
             } else if (keyCode === 40) {
                 dropPlayer()
+            } else if( keyCode === 38) {
+                playerRotate(stage, 1)
             }
         }
     }
